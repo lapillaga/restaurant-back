1ce683d14c21ac387df983141b81c633b7658bcc
Luis Pillaga <lapillaga@espe.edu.ec>
2021-10-14 20:33:52 -0500

    first commit

---
+++ src/test/java/com/gallon/restaurant/RestaurantBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gallon.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
